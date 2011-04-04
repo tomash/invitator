3a02b96795808863d4183cb6f6c930319cded905
Tomasz Stachewicz <tomekrs@o2.pl>
2011-04-04 21:11:48 +0200

    avatar preview

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
   validates :fullname, :presence => true
   validates :address, :presence => true
